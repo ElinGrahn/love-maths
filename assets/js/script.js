39e3a8ecb72ea95b367ad30bd1b54c1fcf5ea3a0
ElinGrahn <>
2023-10-11 12:50:05 +0000

    started js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
+
+document.addEventListener(DOMContentLoaded, function(){
+    let buttons = document.getElementsByTagName("button");
+
+    for(let button of buttons){
+        
+    }
+})
+
 function runGame(){
 
 }
